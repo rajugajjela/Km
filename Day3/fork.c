7973b38bcaa18d6f59f7aa883bfcf40b2aa29fbb
rajugajjela <104496293+rajugajjela@users.noreply.github.com>
2023-02-01 11:48:10 +0530

    Delete Day3 directory

--- Day3/fork.c
+++
@@ -1,9 +0,0 @@
-#include<unistd.h>
-#include<stdio.h>
-
-int main(){
-
-    fork();
-    printf("Hello world\n");
-    getchar();
-}
--- Day3/sgetpid.c
+++
@@ -1,9 +0,0 @@
-#include<sys/types.h>
-#include<unistd.h>
-#include<stdio.h>
-
-int main(){
-    printf("My pid=%d \n", getpid(), getppid());
-    getchar();
-    return (0);
-}
