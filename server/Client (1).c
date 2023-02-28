adff463553bbfba8c7490225407e34373f42ca5b
rajugajjela <104496293+rajugajjela@users.noreply.github.com>
2023-02-28 18:23:56 +0530

    new files

---
+++ server/Client (1).c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+#define PORT 4545
+
+int main(){
+
+	int clientsocket,ret;
+	struct sockaddr_in cliAddr,serverAddr;
+	char buffer[1024];
+
+	clientsocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(clientsocket < 0){
+		printf("error in connection..\n");
+		exit(1);
+	}
+	printf("client socket is created..\n");
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = connect(clientsocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("error in connection...\n");
+		exit(1);
+	}
+	printf("connect to server..\n");
+
+	while(1)
+	{
+	        bzero(buffer, sizeof(buffer));
+		printf("To server:\t");
+		int n=0;
+		while((buffer[n++]=getchar())!= '\n');
+		write(clientsocket,buffer,sizeof(buffer));
+	         read(clientsocket, buffer,sizeof(buffer));
+                 printf("server: \t%s\n",buffer);
+	}
+
+
+	return 0;
+}
+
+
--- server/server.c
+++ server/server.c
@@ -1,59 +1,171 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include <pthread.h>
+#include <semaphore.h>
 
+
+#define PORT 4545
+int count=0;
+int client;
 int main(){
-  int welcomeSocket, newSocket, portNum, clientLen, nBytes;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
-  int i;
 
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
+	sem_t x, y;
+	pthread_t tid;
+	pthread_t writerthreads[100];
+	pthread_t readerthreads[100];
+	int readercount = 0;
+	
+	           
+	int sockfd, ret,buffer1[1025];
+	struct sockaddr_in serverAddr;
 
-  portNum = 7891;
-  
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(portNum);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+	socklen_t addr_size;
 
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+	int newsocket;
+	struct sockaddr_in newAddr;
 
-  if(listen(welcomeSocket,5)==0)
-    printf("Listening\n");
-  else
-    printf("Error\n");
+	char buffer[1024];
+	pid_t childpid;
 
-  addr_size = sizeof serverStorage;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0){
+		printf("error in connection..\n");
+		exit(1);
+	}
+	printf("The server socket is created..\n");
 
-  /*loop to keep accepting new connections*/
-  while(1){
-    newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
-    /*fork a child process to handle the new connection*/
-    if(!fork()){
-      nBytes = 1;
-      /*loop while connection is live*/
-      while(nBytes!=0){
-        nBytes = recv(newSocket,buffer,1024,0);
-  
-        for (i=0;i<nBytes-1;i++){
-          buffer[i] = toupper(buffer[i]);
-        }
 
-        send(newSocket,buffer,nBytes,0);
-      }
-      close(newSocket);
-      exit(0);
-    }
-    /*if parent, close the socket and go back to listening new requests*/
-    else{
-      close(newSocket);
-    }
-  }
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  return 0;
+	ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("error in binding.\n");
+		exit(1);
+	}
+	printf("bind to port %d\n",PORT);
+        if(listen(sockfd, 10)==0){
+		printf("listening..\n");
+	}
+	else
+	{
+		printf("error in binding..\n");
+	}
+
+	while(1){
+		newsocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+		if(newsocket < 0){
+			exit(1);
+		}
+		printf("connection accepted from %s:%d\n",inet_ntoa(newAddr.sin_addr),ntohs(newAddr.sin_port));
+
+		int choice=0;
+		recv(newsocket,&choice,sizeof(choice),0);
+		
+		if(choice==1){
+			if(pthread_create(&writerthread[i++],NULL,reader,&newsocket)!=0)
+			{
+				printf("failed to create");
+			}
+		else if (choice == 2) {
+            // Create writers thread
+        	    if (pthread_create(&writerthreads[i++], NULL,
+        	                       writer, &newsocket)
+        	        != 0)
+ 
+                // Error in creating thread
+        	        printf("Failed to create thread\n");
+        	}
+ 	
+        	if (i >= 50) {
+        	    // Update i
+        	    i = 0;
+ 	
+        	    while (i < 50) {
+        	        // Suspend execution of
+        	        // the calling thread
+        	        // until the target
+        	        // thread terminates
+        	        pthread_join(writerthreads[i++],
+        	                     NULL);
+        	        pthread_join(readerthreads[i++],
+        	                     NULL);
+        	    }
+ 	
+        	    // Update i
+        	    i = 0;
+        	}
+//		printf("client : %d\n",count++);
+//		{
+//			switch(count)
+//			{
+//				case 0:printf("from client-0");
+//
+//				        break;
+//				     
+
+//				case 1: printf("client-1");
+				      
+//				        break;
+//				case 2: printf("client-2");
+//
+//				        break;
+
+//				default: printf("multiple client connections..\n");
+
+//				        break;
+//			}
+//		}		
+			if((childpid = fork()) ==0){
+
+			printf("pid =%d\n",getpid());
+			close(sockfd);
+
+			while(1){
+
+			
+
+
+				bzero(buffer,sizeof(buffer));
+
+
+				//recv(newsocket, buffer,1024,0);
+			        read(newsocket,buffer,sizeof(buffer));	
+					for(int i=0; buffer[i]!='\0'; i++)
+{
+					if(buffer[i] >='a' && buffer[i] <='z')
+					{
+						buffer[i] = buffer[i] -32;
+					}
+					else if(buffer[i]>='A' && buffer[i]<='Z')
+					{
+						buffer[i] = buffer[i]+32;
+					}
+                               
+}
+				printf(" To server %s \t",buffer);
+				
+
+
+		
+			 
+				bzero(buffer,sizeof(buffer));
+				int n=0;
+				while((buffer[n++]=getchar())!='\n');
+                                write(newsocket,buffer,sizeof(buffer));
+
+			}
+		}
+	}
+	close(newsocket);
+	 
+	return 0;
 }
